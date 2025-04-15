f23d96535087a7615f21ba9f8b2cc486f5e9edf3
nsanzimana250 <nsanzimanaesdras2@gmail.com>
2025-04-15 16:55:08 -0700

    update

--- funiture/settings.py
+++ funiture/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'funiture.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'epHyWzYsoZvhPosmGMWsgIoFdjWyTxeB',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
